83a80cc669b2035bf830f42bb04da49f9b09b76e
Alvan Yona <74092874+AlvanYona@users.noreply.github.com>
2025-04-16 16:05:25 +0700

    feat: get events and get event by slug (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 import { PORT } from "./config/env";
 import { errorMiddleware } from "./middlewares/error.middleware";
+
 import sampleRouter from "./routes/sample.router";
 import authRouter from "./routes/auth.router";
-import cors from "cors";
 
+import cors from "cors";
+import eventRouter from "./routes/event.router";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/events", eventRouter);
 
 app.use("/samples", sampleRouter);
 app.use("/auth", authRouter);
--- src/services/sample/get-samples.service.ts
+++ src/services/sample/get-samples.service.ts
@@ -1,7 +1,11 @@
 import prisma from "../../config/prisma";
 
+
+
+
 export const getSamplesService = async () => {
   // const samples = await prisma.sample.findMany();
   
   // return samples;
 };
+
